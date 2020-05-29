69e7a8e2bfca0fdb2f7d6dd4502a95e59d65f560
Lisheng <lishengyu1986@gmail.com>
2020-05-29 23:53:17 +0800

    app functional works

--- Develop/server.js
+++ Develop/server.js
@@ -5,17 +5,30 @@ var session = require("express-session");
 var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8089;
+
+// sequelize will require index.js as default
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
+
+// express middleware to handle incoming request as strings or arrays
 app.use(express.urlencoded({ extended: true }));
+
+// express middleware to handle incoming request as JSON object
 app.use(express.json());
+
+// express middleware to make static file in public folder accessible to web browser
 app.use(express.static("public"));
+
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+
+// middleware that initialize the passport authentication method
 app.use(passport.initialize());
+
+// middleware that deserialized user's request object
 app.use(passport.session());
 
 // Requiring our routes
